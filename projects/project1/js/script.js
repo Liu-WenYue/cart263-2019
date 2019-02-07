8dd5bb389bad50dc34672be09f728c1a3f5b30f4
Liu WenYue <LiuWY0912@gmail.com>
2019-02-07 01:23:38 -0500

    P1: Added font and opening dialog box

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// Store html elements in variables.
+let gear = $('#gear');
+let sisyphus = $('#sisyphus');
+let coin = $('#coin');
+
+$(document).ready(function() {
+    $("#dialog").dialog();
+});
