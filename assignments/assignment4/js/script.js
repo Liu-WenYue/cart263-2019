68b57a76da913ee60ffe6a0ae151a79937e5c623
Liu WenYue <LiuWY0912@gmail.com>
2019-02-21 16:44:53 -0500

    Asg4: Had spongebob only revert when it drops on the mouth

--- assignments/assignment4/js/script.js
+++ assignments/assignment4/js/script.js
@@ -40,17 +40,24 @@ function setup() {
   $spongebob = $('#spongebob');
   // Make it draggable,
   $spongebob.draggable({
-    revert: true
+    // Set the revert to valid will have spongebob only revert when it dropped
+    // on the droppable object.
+    revert: "valid",
+    // When we start drag spongebob, the buzz and crunch sound will stop,
+    // the spongebob laugh will start playing.
+    start: function() {
+      buzzSFX.pause();
+      crunchSFX.pause();
+      laughSFX.play();
+    }
   });
 
   // Get the mouth element from the page
   $mouth = $('#mouth');
   // Make it droppable
   $mouth.droppable({
-    accept: $fly,
-
     // The drop option specifies a function to call when a drop is completed
-    drop: flyDropped
+    drop: handleDrop
   });
 }
 
@@ -60,22 +67,31 @@ function setup() {
 // In this instance it can only be the fly (it's the only draggable element).
 // The arguments 'event' and 'ui' are automatically passed by jQuery UI and contain
 // helpful information about the event.
-function flyDropped (event,ui) {
-  // When we drop the fly into the mouth we should remove the fly from the page
-  // ui contains a reference to the draggable element that was just dropped in ui.draggable
-  // .remove() removes the select element from the page
-  ui.draggable.remove(); // $fly.remove() would work here too
-  // We should "close the mouth" by changing its image
-  // .attr() lets us change specific attributes on HTML element by specifying the attribute
-  // and then what we want to set it to - in this case the 'src' attribute to the closed image
-  $(this).attr('src','assets/images/mouth-closed.png');
-  // Now the fly is gone we should stop its buzzing
-  buzzSFX.pause();
-  // And start the crunching sound effect of chewing
-  crunchSFX.play();
-  // Use a setInterval to call the chew() function over and over
-  setInterval(chew,250);
+function handleDrop (event,ui) {
+  // If spongebob is dropped on the droppable element, nothing will happen,
+  // and spongebob will be revert back to its previous posiiton.
+  if (ui.draggable.attr('id') === 'spongebob') {
+    return;
+  }
+
+  else {
+    // When we drop the fly into the mouth we should remove the fly from the page
+    // ui contains a reference to the draggable element that was just dropped in ui.draggable
+    // .remove() removes the select element from the page
+    ui.draggable.remove(); // $fly.remove() would work here too
+    // We should "close the mouth" by changing its image
+    // .attr() lets us change specific attributes on HTML element by specifying the attribute
+    // and then what we want to set it to - in this case the 'src' attribute to the closed image
+    $(this).attr('src','assets/images/mouth-closed.png');
+    // Now the fly is gone we should stop its buzzing
+    buzzSFX.pause();
+    // And start the crunching sound effect of chewing
+    crunchSFX.play();
+    // Use a setInterval to call the chew() function over and over
+    let chewAnim = setInterval(chew,250);
+  }
 }
+
 
 // chew()
 //
@@ -86,8 +102,6 @@ function chew () {
   if ($mouth.attr('src') === 'assets/images/mouth-open.png') {
     // If it is, we set the 'src' attribute to the closed mouth
     $mouth.attr('src','assets/images/mouth-closed.png');
-    // and play the crunching
-    crunchSFX.play();
   }
   else {
     // Otherwise the 'src' attribute must have been the closed mouth
