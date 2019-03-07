c2a5c2f69a88088d3b06d4859c00fa1dc747d787
Liu WenYue <LiuWY0912@gmail.com>
2019-03-07 14:57:08 -0500

    P2: Added the game description in the script.js

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -5,6 +5,17 @@
 Something is Wrong on the Internet
 Liu WenYue, 07 March 2019
 
+This game is about a girl who was ignored by her parents and asked to
+watch cartoon online on her own. However, as she watches the video,
+there are videos containing horrible stuff which is not suitable for
+kids. The main concept of this game is to illustrated the dangerous
+and harmfulness of letting kids watching videos online without any
+adult accompany ,and appeal to the parents to pay more attention to
+their kids and spend more time with them.
+
+For more detailed game description and the machanism of the game,
+please refer to the README.md file located in this project file.
+
 *********************************************************************/
 
 // Get setup!
