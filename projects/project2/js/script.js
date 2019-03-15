0b8d576cf1221b436c44b018bdab51cb3a1b1d92
Liu WenYue <LiuWY0912@gmail.com>
2019-03-14 20:30:41 -0400

    P2: Reduced the time for each new round to increase the difficulty

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -89,7 +89,7 @@ $(document).ready(function () {
         fill: "tomato",
         thickness: 10,
         lineCap: "round",
-        animation: { duration: 4000 },
+        animation: { duration: 2000 },
         animationStartValue: 0.0,
       });
 
