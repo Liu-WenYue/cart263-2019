e59a600e2e8ee13b992c73d8c69984d9ba615c98
Liu WenYue <LiuWY0912@gmail.com>
2019-04-01 17:38:20 -0400

    Asg8: Created new scene, camera and renderer

--- assignments/assignment8/js/script.js
+++ assignments/assignment8/js/script.js
@@ -9,3 +9,22 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+// Declare the variables for the scene, camera and renderer.
+let scene;
+let camera;
+let renderer;
+
+// Get the document set up.
+$(document).ready(function() {
+  // Create a new scene, a perspective camera and a WEBGL renderer.
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer();
+
+  // Set the size of the renderer to the brower window size.
+  renderer.setSize(window.innerWidth , window.innerHeight);
+
+  // Add the renderer element to the html document.
+  document.body.appendChild(renderer.domElement);
+})
