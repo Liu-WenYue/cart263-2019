756b16e7a6cc4776caee2a2bc2cc48eabee6417b
Liu WenYue <LiuWY0912@gmail.com>
2019-04-03 17:10:12 -0400

    Asg8: Imported a 3d model with texture using OBJLoarder and MTLLoader

--- assignments/assignment8/js/script.js
+++ assignments/assignment8/js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
   camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   renderer = new THREE.WebGLRenderer();
 
-  // Set the background color of the scene.
+  // Set the background color of the scene to white.
   scene.background = new THREE.Color( 0xffffff );
 
   // Set the size of the renderer to the brower window size.
@@ -44,9 +44,51 @@ $(document).ready(function() {
   // Add the plane to the scene.
   scene.add(plane);
 
+  // Use the mtl loader to load the material for the model.
+  let mtlLoader = new THREE.MTLLoader();
+  // Load the material.
+  mtlLoader.setPath('assets/models/');
+  mtlLoader.load('tree.mtl', function(materials) {
+    // Preload the material.
+    materials.preload();
+
+    // Use the obj loader to load the obj file.
+    let objLoader = new THREE.OBJLoader();
+    // Set the material for the obj file,.
+    objLoader.setMaterials(materials);
+    // Find the path for the obj file.
+    objLoader.setPath('assets/models/')
+    objLoader.load(
+      // resource URL
+      'tree.obj',
+      // called when resource is loaded
+      function ( object ) {
+        scene.add( object );
+      },
+
+      // called when loading is in progresses
+      function ( xhr ) {
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+      },
+
+      // called when loading has errors
+      function ( error ) {
+        console.log( 'An error happened' );
+      });
+  });
+
   // Set the camera z position.
   camera.position.z = 10;
 
-  // Render the renderer.
-  renderer.render(scene, camera);
+  // Call animate function.
+  animate();
 })
+
+
+// animate()
+//
+// Function that renders the animation repeatly.
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
