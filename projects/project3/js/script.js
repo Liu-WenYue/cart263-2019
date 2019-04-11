04ea12d46a0414e0a41e0a79893c57dcd6d8c59f
Liu WenYue <LiuWY0912@gmail.com>
2019-04-11 14:20:19 -0400

    P3: Added a plane in the scene

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -14,6 +14,9 @@ let scene;
 let camera;
 let renderer;
 
+// Variable that stores the plane geomotry.
+let plane;
+
 
 // Get the document set up.
 $(document).ready(function() {
@@ -22,9 +25,39 @@ $(document).ready(function() {
   camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
   renderer = new THREE.WebGLRenderer();
 
+
+  // Set the background color of the scene to white.
+  scene.background = new THREE.Color( 0xffffff );
+
   // Set the size of the renderer to the brower window size.
-  renderer.setSize(window.innerWidth , window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
   // Add the renderer element to the html document.
   document.body.appendChild(renderer.domElement);
+
+  // Add plane geometry and material.
+  let planeGeometry = new THREE.PlaneGeometry(100,100,10,10);
+  let planeMaterial = new THREE.MeshBasicMaterial({color:0xcccccc});
+  // Create plane using plane geometry and material.
+  plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  // Rotate the plane.
+  plane.rotation.x -= Math.PI/2;
+
+  // Add the plane to the scene.
+  scene.add(plane);
+
+  // Set the default camera position.
+  camera.position.set(0,2,6);
+
+  // Call animate function.
+  animate();
 })
+
+
+// animate()
+//
+// Function that renders the animation repeatly.
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
